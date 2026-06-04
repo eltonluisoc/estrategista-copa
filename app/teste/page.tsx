1317b4f9f35eff832b1ba1a00ad4d43c33ff9aa1
eltonluisoc <eltonluisoc@gmail.com>
2026-06-04 11:47:52 -0300

    feat: página de teste para verificar build

---
+++ app/teste/page.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+export default function TestePage() {
+  const vencedorId = 123;
+  return (
+    <div>
+      <h1>Teste de Build</h1>
+      <p>vencedorId: {vencedorId}</p>
+      <p>Versão com correção</p>
+    </div>
+  );
+}
